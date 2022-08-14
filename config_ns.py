dabe78712910035a6e8d760e06622dafce1e3ee3
azibit <abdulazeezidris28@gmail.com>
2022-08-14 12:10:43 -0500

    Update config files

--- config_ns.py
+++ config_ns.py
@@ -2,10 +2,11 @@ lr = 0.1 #type=float, help='learning rate')
 resume = False # help='resume from checkpoint')
 model = "ResNet18" #type=str, help='model type (default: ResNet18)')
 # dataset_dir = '../Kvasir - Aziz_224' # type=str, help='The location of the dataset to be explored')
-dataset_dir = '../DS1'
+# dataset_dir = '../DS1'
+dataset_dir = '../Extra_Datasets'
 subset_folder = 'SUBSET_FOLDER' # type=str, help='Temporary location for dataset')
-trials = 1 # type=int, help='Number of times to run the complete experiment')
-iterations = 1 # type=int, help='Number of times to run the complete experiment')
+trials = 2 # type=int, help='Number of times to run the complete experiment')
+iterations = 4 # type=int, help='Number of times to run the complete experiment')
 epochs = 200 # type=int, help='Epochs')
 name = '0' # type=str, help='name of run')
 seed = 0 # type=int, help='random seed')
@@ -14,7 +15,9 @@ batch_size = 128 # type=int, help='batch size')
 no_augment = False # help='use standard augmentation (default: True)')
 alpha = 1. # type=float, help='mixup interpolation coefficient (default: 1)')
 # image_size = 224 # type=int, help='input image size')
-image_size = 32
+# image_size = 32
+# image_size=96
+image_size=28
 use_old = True # help='Use old code base')
 ns_epochs = 200 # type=int, help='ns_epochs')
 subset_train_iter = 1 # type=int, help='Iterations for the subset training model')
